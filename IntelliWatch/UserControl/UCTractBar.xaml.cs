5884b21fe9a664729381c320b092b79e6e9ed14c
ZoeCheck <893622396@qq.com>
2014-09-30 00:42:28 +0800

    初步制作了TrackBar的样式；调整了日期输入框的样式

---
+++ IntelliWatch/UserControl/UCTractBar.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntelliWatch
+{
+	/// <summary>
+	/// UCTractBar.xaml 的交互逻辑
+	/// </summary>
+	public partial class UCTractBar : UserControl
+	{
+		public UCTractBar()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
