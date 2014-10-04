ea231927c18e81885e3809db38aa60cef3eabadf
ZoeCheck <893622396@qq.com>
2014-10-04 13:38:08 +0800

    修改了小屏控制按钮组为鼠标移上就显示

--- IntelliWatch/UserControl/UCSmallScreen.xaml.cs
+++ IntelliWatch/UserControl/UCSmallScreen.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace IntelliWatch
 {
@@ -18,9 +19,32 @@ namespace IntelliWatch
 	/// </summary>
 	public partial class UCSmallScreen : UserControl
 	{
+		public bool IsMouseOver
+		{
+			get { return (bool)GetValue(IsMouseOverProperty); }
+			set { SetValue(IsMouseOverProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for IsMouseOver.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty IsMouseOverProperty =
+			DependencyProperty.Register("IsMouseOver", typeof(bool), typeof(UCSmallScreen), new UIPropertyMetadata(false));
+
+
+
 		public UCSmallScreen()
 		{
 			this.InitializeComponent();
+			this.DataContext = this;
+		}
+
+		private void UserControl_MouseEnter(object sender, MouseEventArgs e)
+		{
+			IsMouseOver = true;
+		}
+
+		private void UserControl_MouseLeave(object sender, MouseEventArgs e)
+		{
+			IsMouseOver = false;
 		}
 	}
 }
